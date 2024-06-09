7cf8c6a0173048a05b76ce812dd20206b3633bc8
Grisha Petrov <63047435+gptrv@users.noreply.github.com>
2024-06-09 13:39:20 +0500

    platforms (#3)
    Co-authored-by: gptrv <g.petrov@artsofte.ru>

--- proxy-app/src/app/app.component.ts
+++ proxy-app/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPage } from './pages/main/main.page';
+import { HeaderComponent } from "./components/header/header.componet";
+import { AndroidPage } from "./children/platforms/pages/android/android.page";
+import { IosPage } from "./children/platforms/pages/ios/ios.page";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        MainPage,
+        HeaderComponent,
+        AndroidPage,
+        IosPage
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'proxy-app';
+    title = 'proxy-app';
 }
---
+++ proxy-app/src/app/pages/main/main.page.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    templateUrl: './main.page.html',
+    styleUrls: [],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MainPage {
+
+}
